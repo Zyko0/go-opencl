ba792633555ba356cd7e03cc994d796192bd6c05
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2024-02-29 17:03:42 +0100

    add multiple paths

--- library_notwindows.go
+++ library_notwindows.go
@@ -8,24 +8,51 @@ import (
 	"runtime"
 )
 
-func getOpenCLPath() (string, error) {
-	switch runtime.GOOS {
-	case "linux":
-		return "libOpenCL.so", nil
-	default:
-		return "", errors.New("unsupported operating system")
+func getOpenCLPath() []string {
+	if runtime.GOOS == "linux" {
+		return []string{
+			"/usr/lib/libOpenCL.so",
+			"/usr/local/lib/libOpenCL.so",
+			"/usr/local/lib/libpocl.so",
+			"/usr/lib64/libOpenCL.so",
+			"/usr/lib32/libOpenCL.so",
+			"libOpenCL.so"}
+	} else if runtime.GOOS == "darwin" {
+		return []string{
+			"libOpenCL.so",
+			"/System/Library/Frameworks/OpenCL.framework/OpenCL"}
+	} else if runtime.GOOS == "android" {
+		return []string{
+			"/system/lib64/libOpenCL.so",
+			"/system/vendor/lib64/libOpenCL.so",
+			"/system/vendor/lib64/egl/libGLES_mali.so",
+			"/system/vendor/lib64/libPVROCL.so",
+			"/data/data/org.pocl.libs/files/lib64/libpocl.so",
+			"/system/lib/libOpenCL.so",
+			"/system/vendor/lib/libOpenCL.so",
+			"/system/vendor/lib/egl/libGLES_mali.so",
+			"/system/lib64/egl/libGLES_mali.so",
+			"/system/vendor/lib/libPVROCL.so",
+			"/data/data/org.pocl.libs/files/lib/libpocl.so",
+			"libOpenCL.so"}
 	}
+	return make([]string, 0)
 }
 
 func loadLibrary() (uintptr, error) {
-	path, err := getOpenCLPath()
-	if err != nil {
-		return 0, err
+	paths := getOpenCLPath()
+	if len(paths) == 0 {
+		return 0, errors.New("unknown system paths")
 	}
-	handle, err := purego.Dlopen(path, purego.RTLD_NOW|purego.RTLD_GLOBAL)
-	if err != nil {
-		return 0, err
-	}
+	return getLib(paths)
+}
 
-	return handle, err
+func getLib(paths []string) (uintptr, error) {
+	for i := 0; i < len(paths); i++ {
+		libOpenCl, err := purego.Dlopen(paths[i], purego.RTLD_NOW|purego.RTLD_GLOBAL)
+		if err == nil {
+			return libOpenCl, err
+		}
+	}
+	return 0, errors.New("no path has passed")
 }
