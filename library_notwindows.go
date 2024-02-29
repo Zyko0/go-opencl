ed6bc8a497b49da2045b91c2cd7c6beb65f7edd3
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2024-02-29 22:05:42 +0100

    last refactor

--- library_notwindows.go
+++ library_notwindows.go
@@ -8,19 +8,25 @@ import (
 	"runtime"
 )
 
-func getOpenCLPath() []string {
-	if runtime.GOOS == "linux" {
+import (
+	"errors"
+	"github.com/ebitengine/purego"
+	"runtime"
+)
+
+func getOpenCLPath() ([]string, error) { // these conditions will compiler solve at compile time and function will probably inline
+	if runtime.GOOS == "linux" || runtime.GOOS == "freebsd" {   
 		return []string{
 			"/usr/lib/libOpenCL.so",
 			"/usr/local/lib/libOpenCL.so",
 			"/usr/local/lib/libpocl.so",
 			"/usr/lib64/libOpenCL.so",
 			"/usr/lib32/libOpenCL.so",
-			"libOpenCL.so"}
+			"libOpenCL.so"}, nil
 	} else if runtime.GOOS == "darwin" {
 		return []string{
 			"libOpenCL.so",
-			"/System/Library/Frameworks/OpenCL.framework/OpenCL"}
+			"/System/Library/Frameworks/OpenCL.framework/OpenCL"}, nil
 	} else if runtime.GOOS == "android" {
 		return []string{
 			"/system/lib64/libOpenCL.so",
@@ -34,15 +40,15 @@ func getOpenCLPath() []string {
 			"/system/lib64/egl/libGLES_mali.so",
 			"/system/vendor/lib/libPVROCL.so",
 			"/data/data/org.pocl.libs/files/lib/libpocl.so",
-			"libOpenCL.so"}
+			"libOpenCL.so"}, nil
 	}
-	return make([]string, 0)
+	return nil, errors.New("unknown system paths")
 }
 
 func loadLibrary() (uintptr, error) {
-	paths := getOpenCLPath()
-	if len(paths) == 0 {
-		return 0, errors.New("unknown system paths")
+	paths, err := getOpenCLPath()
+	if err != nil {
+		return 0, err
 	}
 	for i := 0; i < len(paths); i++ {
 		libOpenCl, err := purego.Dlopen(paths[i], purego.RTLD_NOW|purego.RTLD_GLOBAL)
